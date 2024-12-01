94264356b3d5a20d71a304072f5ec6210b09ba8c
Steven Jiang <stevenjiangnz@gmail.com>
2024-12-01 18:21:47 +1000

    update build.sh

--- python/simpler-logger/build/build.sh
+++ python/simpler-logger/build/build.sh
@@ -3,16 +3,53 @@
 # Navigate to the directory containing this script
 cd "$(dirname "$0")"
 
+# Default image name
+DEFAULT_IMAGE_NAME="simple-logger"
+
+# Check if an image name was provided as an argument
+IMAGE_NAME=${1:-$DEFAULT_IMAGE_NAME}
+
+# Display which image name will be used
+echo "Using image name: $IMAGE_NAME"
+
 # Build the Docker image
 echo "Building Docker image..."
-docker build -t simple-logger -f Dockerfile ..
+docker build -t "$IMAGE_NAME" -f Dockerfile ..
 
 # Check if build was successful
 if [ $? -eq 0 ]; then
     echo "Build successful!"
-    echo "To run the container, use: docker run simple-logger"
-    echo "To run in detached mode, use: docker run -d simple-logger"
+    echo "To run the container, use: docker run $IMAGE_NAME"
+    echo "To run in detached mode, use: docker run -d $IMAGE_NAME"
     echo "To view logs, use: docker logs -f <container_id>"
 else
     echo "Build failed!"
+    exit 1
+fi#!/bin/bash
+
+# Navigate to the directory containing this script
+cd "$(dirname "$0")"
+
+# Default image name
+DEFAULT_IMAGE_NAME="simple-logger"
+
+# Check if an image name was provided as an argument
+IMAGE_NAME=${1:-$DEFAULT_IMAGE_NAME}
+
+# Display which image name will be used
+echo "Using image name: $IMAGE_NAME"
+
+# Build the Docker image
+echo "Building Docker image..."
+docker build -t "$IMAGE_NAME" -f Dockerfile ..
+
+# Check if build was successful
+if [ $? -eq 0 ]; then
+    echo "Build successful!"
+    echo "To run the container, use: docker run $IMAGE_NAME"
+    echo "To run in detached mode, use: docker run -d $IMAGE_NAME"
+    echo "To view logs, use: docker logs -f <container_id>"
+else
+    echo "Build failed!"
+    exit 1
 fi
