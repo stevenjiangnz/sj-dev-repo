b69fde4dce6cecb03a89709904f90c731f55198f
Steven Jiang <stevenjiangnz@gmail.com>
2024-12-01 21:12:41 +1000

    add py

--- python/simpler-logger/src/logger.py
+++ python/simpler-logger/src/logger.py
@@ -8,7 +8,7 @@ sys.stdout.reconfigure(line_buffering=True)
 def log_message():
     while True:
         current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"[{current_time}] Hello! I'm still running!", flush=True)
+        print(f"[{current_time}] Hello! I'm still running - ok!", flush=True)
         time.sleep(3)  # Wait for 3 seconds
 
 if __name__ == "__main__":
